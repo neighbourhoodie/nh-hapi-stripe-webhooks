a3840c5f0dc37feba8bd07df25cf7c5aacc22678
hulkoba <fuchspippi@web.de>
2019-08-01 13:56:30 +0200

    chore: add logs for testing

--- index.js
+++ index.js
@@ -1,29 +1,50 @@
 'use strict'
 
-const {
-  createSubscription,
-  updateSubscription,
-  removeSubscription
-} = require('lib/subscriptions')
+// const {
+//   createSubscription,
+//   updateSubscription,
+//   removeSubscription
+// } = require('lib/subscriptions')
 
-const {
-  createCustomer,
-  updateCustomer,
-  removeCustomer
-} = require('lib/customers')
+// const {
+//   createCustomer,
+//   updateCustomer,
+//   removeCustomer
+// } = require('lib/customers')
 
-const webhookHandlers = {
-  'payments.subscription.create': createSubscription,
-  'payments.subscription.update': updateSubscription,
-  'payments.subscription.remove': removeSubscription,
-  'payments.customer.create': createCustomer,
-  'payments.customer.update': updateCustomer,
-  'payments.customer.remove': removeCustomer
+// const webhookHandlers = {
+//   'payments.subscription.create': createSubscription,
+//   'payments.subscription.update': updateSubscription,
+//   'payments.subscription.remove': removeSubscription,
+//   'payments.customer.create': createCustomer,
+//   'payments.customer.update': updateCustomer,
+//   'payments.customer.remove': removeCustomer
+// }
+
+/*
+options: {
+  stripeSecret: 'i like broccoli',
+  observe: ['event1', 'event2']
 }
-
+*/
 exports.plugin = {
   pkg: require('./package.json'),
   register: async function (server, options) {
-    return webhookHandlers
+    console.log('### hello ###')
+    // await webhookHandlers
+
+    // var stripe = createStripeClient(options.stripeSecretKey)
+    server.route({
+      config: {
+        id: 'stripe-webhooks',
+        handler: function (request, h) {
+          console.log('### options', options)
+          return options.name
+        }
+      },
+      method: 'POST',
+      path: options.path
+    })
+    // return next()
   }
 }
--- readme.md
+++ readme.md
@@ -1,2 +1,4 @@
 # The Neighbourhoodie Hapi Stripe webhooks
-A Hapi plugin for receiving notifications from the Stripe webhooks API.
+A [Hapi](https://hapi.dev/) plugin for receiving notifications from the [Stripe webhooks](https://stripe.com/docs/webhooks) API.
+
+Stripe can send webhook events that notify your application any time an event happens on your account. The webhooks endpoint will need to authenticate webhooks as per stripe docs.
